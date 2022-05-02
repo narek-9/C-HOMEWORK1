a4b06509f915e627ed27d1a14cf4b210ccfa5579
Narek <narekazibekyan5@gmail.com>
2022-05-02 18:52:17 +0400

    max number

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1 ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2 ");
+int b = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > b) max = a; 
+else max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
