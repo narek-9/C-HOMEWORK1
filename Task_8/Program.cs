2f38a893eb66474ff8bf1a3ce15eb8e1a1a8abfe
Narek <narekazibekyan5@gmail.com>
2022-05-02 19:55:33 +0400

    all even numbers

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите положительное число ");
+
+int N = int.Parse(Console.ReadLine());
+int Y = 2;
+
+while (Y <= N)
+{
+    Console.Write(Y + ",");
+    Y = Y + 2;
+}
+
+if (N <= 1) 
+{
+    Console.WriteLine("Четных чисел нет ");
+}
