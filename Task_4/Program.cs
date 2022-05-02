7288ae4afdaf344edd54425169666bf0b36780bc
Narek <narekazibekyan5@gmail.com>
2022-05-02 19:36:15 +0400

    max of three

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число 1 ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2 ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 3 ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > b) 
+     if(a > c)
+    max = a;
+
+
+if (b > a) 
+     if(b > c)
+    max = b;
+
+if (c > a) 
+     if(c > b)
+    max = c;
+
+Console.WriteLine(max);
