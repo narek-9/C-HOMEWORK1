945bebe65aa2a194c25cef902883e26e18392ecb
Narek <narekazibekyan5@gmail.com>
2022-05-02 19:38:41 +0400

    yes or no

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+
+double num = double.Parse(Console.ReadLine());
+
+if (num % 2 ==1 )
+{
+    Console.WriteLine("нет");
+}
+else {
+    Console.WriteLine("да");
+}
